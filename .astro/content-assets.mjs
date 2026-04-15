2566a5cb3bdd3d58a5c041ad160120200fbdbea4
James Evans <james@jamesburtonevans.com>
2026-04-15 12:22:03 -0400

    Updated resume and selected coursework case study.

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,6 @@
+
+import __ASTRO_IMAGE_IMPORT_Zgn78U from "./images/cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fcorvus-echoes-landing-page%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_ZgEFDp from "./images/cover_unca.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fselected-coursework%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_27R5aF from "./images/endelboost-cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fendel-boost%2Findex.mdx";
+export default new Map([["./images/cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fcorvus-echoes-landing-page%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Zgn78U], ["./images/cover_unca.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fselected-coursework%2Findex.mdx", __ASTRO_IMAGE_IMPORT_ZgEFDp], ["./images/endelboost-cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fendel-boost%2Findex.mdx", __ASTRO_IMAGE_IMPORT_27R5aF]]);
+		
---
+++ .astro/content-modules.mjs
@@ -0,0 +1,6 @@
+
+export default new Map([
+["src/content/projects/corvus-echoes-landing-page/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fcorvus-echoes-landing-page%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/projects/endel-boost/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fendel-boost%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/projects/selected-coursework/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fselected-coursework%2Findex.mdx&astroContentModuleFlag=true")]]);
+		
